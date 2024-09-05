55fed171cabf549eabed63ade0052b209ae1ac6c
Cecilia <ravacecilia1@gmail.com>
2024-09-05 19:08:55 -0300

    feat: Create the GET method

--- app.js
+++ app.js
@@ -9,21 +9,45 @@ const CONNECTION = MYSQL.createConnection({
     user: 'root',
     password: '',
     database: 'productsdb'
-})
+});
 
 //Test the connection
 CONNECTION.connect(function(error){
     if(error){
         throw error;
     } else {
-        console.log('Connected with the database')
+        console.log('Connected with the database');
     }
-})
+});
 
 
 APP.get('/', function(req, res){
     res.send('Init route')
-})
+});
+
+
+//Show all the products
+APP.get('/api/products', (req,res) => {
+    CONNECTION.query('SELECT * FROM products', (error, rows) => {
+        if(error){
+            throw error;
+        } else {
+            res.send(rows);
+        }
+    })
+});
+
+//Show a particular product
+APP.get('/api/products/:id', (req,res) => {
+    CONNECTION.query('SELECT * FROM products WHERE id = ?', [req.params.id], (error, row) => {
+        if(error){
+            throw error;
+        } else {
+            res.send(row);
+        }
+    })
+});
+
 
 const PORT = process.env.PORT || 3000;
 
